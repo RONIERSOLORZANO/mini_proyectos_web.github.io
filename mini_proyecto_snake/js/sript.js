e3f6744276f21cf3a844145e51f3ae133ec90cec
RONIERSOLORZANO <roniersolorzano@outlook.es>
2024-02-04 21:39:32 -0400

    fin proyecto mini_proyecto_cronometro

---
+++ mini_proyecto_snake/js/sript.js
@@ -0,0 +1,38 @@
+const board = document.getElementById("board");
+const scoreBoard = document.getElementById("scoreBoard");
+const startButton = document.getElementById("start");
+const gameOver = document.getElementById("gameOver");
+
+const boardSize = 10;
+const gameSpeed = 100;
+const squareTypes = {
+    emptySquare:0,
+    snakeSquare: 1,
+    foodSquare: 2
+};
+
+const directions = {
+    ArrowUp: -10,
+    ArrowDown: 10,
+    ArrowRight: 1,
+    ArrowLeft: -1
+};
+
+let snake;
+let score;
+let direction;
+let boardSquares;
+let emptySquares;
+let moveInterval;
+
+const setGame = () => {
+    snake = ('00', '01', '02', '03');
+    score = snake.length;
+    direction = 'ArrowRight'
+}
+
+const startGame = () => {
+    setGame();
+}
+
+startButton.addEventListener("click", startGame)
